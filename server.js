2eaa7207fc1096dca17966755387b10e8e23bb51
chinwizu <chinwianthony@gmail.com>
2023-01-02 06:37:18 +0100

    basic routing

--- server.js
+++ server.js
@@ -3,16 +3,42 @@ const fs =require('fs');
 const server = http.createServer((req,res) => {
     // setting response header
    res.setHeader('content-Type', 'text/html');
-//    to send a file as response, we have to read, then send.
-    fs.readFile('./pages/index.html', (err, data) => {
+    // after setting the header, we create cases for the files using switch
+
+    // let's create a path first, where the files are located
+    let path = './pages/';
+
+    // let's now do the switching
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+
+        case '/about':
+            path += 'about.html';
+            break;
+
+        case '/contact':
+            path += 'contact.html'
+            break;
+
+        default:
+            path += '404.html'
+            break;
+    }
+
+    // after setting the cases, we then modify the sending code
+
+    fs.readFile(path,(err,data) =>{
         if(err) {
             console.log(err);
-            res.end();
-        } else {
+            res.send();
+        }else{
             res.write(data);
             res.end();
         }
     });
+
     
 });
 
